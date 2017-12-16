e3c9e82747f7358ff6b2449237d5c55b3ce8f55e
Fabian Beuke <mail@beuke.org>
2017-12-16 19:45:59 +0100

    add travis ci

--- test.sh
+++ test.sh
@@ -1,1 +1,14 @@
+set -xe
+
+node index.js
 node index.js --amount 0.5 --from BTC --to USD
+node index.js --amount 0.5 --from BTC --to BTC
+node index.js -a 0.5 -f BTC -t BTC
+node index.js --amount 0.1 --from BTC --to ADA
+node index.js --amount 0.1 --from ETH --to USD
+node index.js --amount 2 --from BTC --to ADA
+node index.js --amount 2 --from IOT --to USD
+node index.js --amount 2 --from XRP --to BTC
+node index.js --amount 2 --from XRP
+node index.js --amount 2 --to BTC
+node index.js --amount 2
