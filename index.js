bc7a027655b7c7a4aef24bcf9e8b76a24c9ff584
Fabian Beuke <mail@beuke.org>
2017-12-16 19:14:03 +0100

    add lint

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const param = require('commander')
 const {version} = require('./package.json')
-const {get, defaultTo, map, flow, sortBy,
-    remove, chunk, ceil, mean, toLower, trim,
-    negate, first, pad, max} = require('lodash/fp')
+const {first} = require('lodash/fp')
 const axios = require('axios')
 const print = string => process.stdout.write(string + '\n')
 
 param
-  .version(version)
-  .option('-a, --amount <n>', 'number of coins')
-  .option('-f, --from <string>', 'specify the coin to convert from e.g. ETH')
-  .option('-t, --to <string>', 'specify the coin to convert to e.g. BTC')
-  .parse(process.argv)
+    .version(version)
+    .option('-a, --amount <n>', 'number of coins')
+    .option('-f, --from <string>', 'specify the coin to convert from e.g. ETH')
+    .option('-t, --to <string>', 'specify the coin to convert to e.g. BTC')
+    .parse(process.argv)
 
 const api = `https://min-api.cryptocompare.com/data/price?fsym=${param.from}&tsyms=${param.to}`
 
 const main = async () => {
-  const result = (await axios.get(api)).data
-  const val = first(Object.values(result))
-  const currency = first(Object.keys(result))
-  print((val * (param.amount ? param.amount : 1)) + " " + currency)
+    const result = (await axios.get(api)).data
+    const val = first(Object.values(result))
+    const currency = first(Object.keys(result))
+    return print((val * (param.amount ? param.amount : 1)) + ' ' + currency)
 }
 
 main()
