ebf5242ba76e003aecf2371953a91fd8aa712e33
Fabian Beuke <mail@beuke.org>
2021-05-09 13:15:27 +0200

    remove eslint; format with prettier defaults

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
 
-const param = require('commander')
-const {version} = require('./package.json')
-const {first} = require('lodash/fp')
-const axios = require('axios')
-const print = string => process.stdout.write(string + '\n')
+const param = require("commander");
+const { version } = require("./package.json");
+const { first } = require("lodash/fp");
+const axios = require("axios");
+const print = (string) => process.stdout.write(string + "\n");
 
 param
     .version(version)
-    .option('-a, --amount <n>', 'number of coins')
-    .option('-f, --from <string>', 'specify the coin to convert from e.g. ETH')
-    .option('-t, --to <string>', 'specify the coin to convert to e.g. BTC')
-    .parse(process.argv)
+    .option("-a, --amount <n>", "number of coins")
+    .option("-f, --from <string>", "specify the coin to convert from e.g. ETH")
+    .option("-t, --to <string>", "specify the coin to convert to e.g. BTC")
+    .parse(process.argv);
 
-const from = param.from ? param.from : 'BTC'
-const to = param.to ? param.to : 'USD'
+const from = param.from ? param.from : "BTC";
+const to = param.to ? param.to : "USD";
 
-const api = `https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}`
+const api = `https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}`;
 
 const main = async () => {
-    const result = (await axios.get(api)).data
-    const val = first(Object.values(result))
-    const currency = first(Object.keys(result))
-    return print((val * (param.amount ? param.amount : 1)) + ' ' + currency)
-}
+    const result = (await axios.get(api)).data;
+    const val = first(Object.values(result));
+    const currency = first(Object.keys(result));
+    return print(val * (param.amount ? param.amount : 1) + " " + currency);
+};
 
-main()
+main();
