a9c893f0dacd7aa824bb8344e318f802d1011a26
Fabian Beuke <mail@beuke.org>
2021-05-09 13:22:24 +0200

    update to commander 7.2

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 #!/usr/bin/env node
 
-const param = require("commander");
+const { Command } = require('commander');
 const { version } = require("./package.json");
 const { first } = require("lodash/fp");
 const axios = require("axios");
 const print = (string) => process.stdout.write(string + "\n");
 
-param
+const program = new Command();
+program
     .version(version)
     .option("-a, --amount <n>", "number of coins")
     .option("-f, --from <string>", "specify the coin to convert from e.g. ETH")
     .option("-t, --to <string>", "specify the coin to convert to e.g. BTC")
     .parse(process.argv);
 
+const param = program.opts();
 const from = param.from ? param.from : "BTC";
 const to = param.to ? param.to : "USD";
 
